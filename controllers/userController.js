b03d9b39ac0132a3f64c6061269b17cd10e52856
Alex <alexsp2002@outlook.es>
2022-04-20 11:57:54 +0200

    Primer commit aprovechando recursos de otra api

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const User = require('../model/userModel');
+
+function standar(req, res) {
+  return res.json({ text: 'Main Page' });
+}
+
+function getUsuarioById(req, res) {
+  User.findById(req.params.id, (err, userData) => {
+    if (err) {
+      return res.status(400).send(err.message);
+    }
+    return res.send(userData);
+  });
+}
+
+function getUserByEmail(req, res) {
+  User.findOne({ email: req.params.email }, (err, userData) => {
+    if (err) return res.status(400).send(`Error in email search: ${err.message}`);
+    return res.send(userData);
+  });
+}
+
+function createUser(req, res) {
+  const newUser = new User(req.body);
+
+  newUser.save((err, userInfo) => {
+    if (err) return res.status(400).send(`Error in controller: ${err.message}`);
+    return res.status(200).send(userInfo);
+  });
+}
+function updateUser(req, res) {
+  User.findByIdAndUpdate(req.params.id, req.body, (err, updatedUser) => {
+    if (err) return res.status(400).send(`Failed to update: ${err.message}`);
+    return res.status(200).send({ message: 'update ok', updatedUser });
+  });
+}
+function deleteUser(req, res) {
+  User.findByIdAndRemove(req.params.id, (err, userDeleted) => {
+    if (err) return res.status(400).send(`Failed to deleted: ${err.message}`);
+    return res.status(200).send({ message: 'User deleted', userDeleted });
+  });
+}
+
+module.exports = {
+  getUsuarioById,
+  createUser,
+  deleteUser,
+  updateUser,
+  getUserByEmail,
+  standar,
+};
---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const { randomBytes, pbkdf2Sync } = require('crypto');
+
+const { Schema } = mongoose;
+
+// Creation of mongoose schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name required'],
+  },
+  surname: {
+    type: String,
+    required: [true, 'Surname required'],
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: [true, 'Password is required'],
+  },
+  saltPass: {
+    type: String,
+  },
+});
+
+function hashPasword(next) {
+  const user = this;
+
+  user.saltPass = randomBytes(16).toString('hex');
+
+  if (user.isModified('password')) {
+    user.password = pbkdf2Sync(user.password, user.saltPass, 100000, 64, 'sha512').toString('hex');
+  }
+  return next();
+}
+function comparePass(pass) {
+  const user = this;
+  return user.password === pbkdf2Sync(pass, user.saltPass, 100000, 64, 'sha512').toString('hex');
+}
+function createEmail(next) {
+  const user = this;
+  user.email = (`${user.name}.${user.surname}@mequieromorir.com`).toLocaleLowerCase();
+  return next();
+}
+
+userSchema.pre('save', hashPasword);
+userSchema.pre('save', createEmail);
+userSchema.methods.comparePass = comparePass;
+
+module.exports = mongoose.model('User', userSchema);
