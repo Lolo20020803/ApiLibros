4e2a0b1c3ce4e84a12e37b026697ddc7f386dc42
Alex <alexsp2002@outlook.es>
2022-04-22 15:15:44 +0200

    Conexion con atlas

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = /*process.env.PORT ||*/ 3000; // Port assign
-const db = /*process.env.MONGO_URI ||*/ 'mongodb://localhost:27017/ApiLibros'; // Database Location
+const PORT = process.env.PORT || 3000; // Port assign
+const db = process.env.MONGO_URI || 'mongodb://localhost:27017/ApiLibros'; // Database Location
 
 mongoose.connect(db, (err) => {
   if (err) console.log(`Cant connect to database. Error: ${err}`);
-  else app.listen(PORT, () => { console.log(`Node server running on http://localhost:${PORT}`); });
+  else app.listen(PORT, () => { console.log(`Node server running on http://localhost:${PORT} using the db:${db}`); });
 });
 
 module.exports = app;
